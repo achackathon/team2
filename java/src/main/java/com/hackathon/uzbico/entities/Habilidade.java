549283b560cbb55b4a4380bcf1ef204f96589876
Maurício Moreira de Aguilar <mmaguylar@hotmail.com>
2016-03-05 16:16:31 -0300

    @entity habilidade

--- java/src/main/java/com/hackathon/uzbico/entities/Habilidade.java
+++ java/src/main/java/com/hackathon/uzbico/entities/Habilidade.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 import com.sun.istack.internal.NotNull;
 
 @Table(name="habilidade")
+@Entity
 public class Habilidade implements Serializable {
 
 	/**
