a1fce55d2ba7ff910772caeaef3ac417eaa1e16f
Maurício Moreira de Aguilar <mmaguylar@hotmail.com>
2016-03-05 16:01:51 -0300

    settings

--- java/src/main/java/com/hackathon/uzbico/service/HabilidadeService.java
+++ java/src/main/java/com/hackathon/uzbico/service/HabilidadeService.java
@@ -1,8 +1,7 @@
 package com.hackathon.uzbico.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,27 +10,28 @@ import com.hackathon.uzbico.entities.Habilidade;
 import com.hackathon.uzbico.repository.HabilidadeRepository;
 
 @RestController
+@RequestMapping("/habilidade")
 public class HabilidadeService {
 
 	@Autowired
 	private HabilidadeRepository repository;
 
-	@RequestMapping(value = "/habilidade/add", method = RequestMethod.POST)
-	public Habilidade save(Habilidade habilidade) {
-		return repository.save(habilidade);
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ResponseEntity<?> save(Habilidade habilidade) {
+		return ResponseEntity.ok(repository.save(habilidade));
 	}
 
-	@RequestMapping(value = "/habilidade/listaTodos", method = RequestMethod.GET)
-	public List<Habilidade> findAll() {
-		return repository.findAll();
+	@RequestMapping(value = "/listaTodos", method = RequestMethod.GET)
+	public ResponseEntity<?> findAll() {
+		return ResponseEntity.ok(repository.findAll());
 	}
 
-	@RequestMapping(value = "/habilidade/atualiza")
+	@RequestMapping(value = "/atualiza", method = RequestMethod.POST)
 	public void updade(Habilidade habilidade) {
 		repository.update(habilidade);
 	}
 
-	@RequestMapping(value = "/habilidade/delete/{id}")
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
